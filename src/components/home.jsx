e13bf3588ba9832bbba41ce38322b891f44de2c1
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-01-17 10:37:45 +0900

    Added a simple title page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,9 +3,13 @@ import NavBar from "./navbar";
 
 function Home(props){
   return (
-    <>
+    <div className={styles.homepage}>
       <NavBar quantity={props.quantity}/>
-    </>
+      <div className={styles.titlediv}>
+        <h1>Welcome to the Shopping Cart App!</h1>
+        <p>Browse our collection of products and add them to your cart.</p>
+      </div>
+    </div>
   ); 
 }
 
